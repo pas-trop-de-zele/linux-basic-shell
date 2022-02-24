9fb4910b6e6d027f1e66bd5d6c7242577ed7f38c
Sam <huy.le09@protonmail.com>
2022-02-24 11:21:27 -0800

    Parser working

--- main.c
+++ main.c
@@ -10,21 +10,28 @@
 
 int min(int a, int b) { return a < b ? a : b; }
 
-// break a string into its tokens, putting a \0 between each token
-//   save the beginning of each string in a string of char *'s (ptrs to chars)
-int parse(char *s, char *argv[])
+int parse(char *input, char *argv[])
 {
-    const char break_chars[] = " \t;";
-    char *p;
-    int c = 0;
-    /* TODO */ // write parser that breaks input into argv[] structure
-    // e.g., cal  -h  2022\0      // would be
-    //       |    |   |
-    //       |  \0| \0|   \0      // '\0' where all the spaces are
-    //       p0   p1  p2          // array of ptrs to begin. of strings ("cal", "-h", "2022")
-    //                            // char* argv[]
+    int argument_start = 0;
+    int arg_no = 0;
+    int i = 0;
 
-    return c; // int argc
+    int input_length = strlen(input);
+    while (i < input_length + 1)
+    {
+        if (input[i] == ' ' || i == input_length)
+        {
+            if (argument_start < i)
+            {
+                input[i] = '\0';
+                argv[arg_no] = input + argument_start;
+                arg_no++;
+            }
+            argument_start = i + 1;
+        }
+        i++;
+    }
+    return arg_no;
 }
 
 // execute a single shell command, with its command line arguments
@@ -36,13 +43,10 @@ int execute(char *input)
     memset(shell_argv, 0, MAX_CMD_LINE_ARGS * sizeof(char));
 
     int shell_argc = parse(input, shell_argv);
-    // printf("after parse, what is input: %s\n", input);      // check parser
-    // printf("argc is: %d\n", shell_argc);
-    // while (shell_argc > 0) {
-    //   printf("argc: %d: %s\n", i, shell_argv[i]);
-    //   --shell_argc;
-    //   ++i;
-    // }
+    for (int i = 0; i < shell_argc; i++)
+    {
+        printf("%s \n", shell_argv[i]);
+    }
 
     int status = 0;
     pid_t pid = fork();
@@ -55,7 +59,7 @@ int execute(char *input)
     { // child
         int ret = 0;
         // if ((ret = execlp("cal", "cal", NULL)) < 0) {  // can do it arg by arg, ending in NULL
-        if ((ret = execvp(*shell_argv, shell_argv)) < 0)
+        if ((ret = execvp(shell_argv[0], shell_argv)) < 0)
         {
             fprintf(stderr, "execlp(%s) failed with error code: %d\n", *shell_argv, ret);
         }
@@ -67,7 +71,6 @@ int execute(char *input)
         {
         }
     }
-
     return 0;
 }
 
@@ -85,18 +88,14 @@ int main(int argc, const char *argv[])
         printf("osh > ");
         fflush(stdout);
 
-        if (strlen(input) > 0)
+        fgets(input, BUFSIZ, stdin);
+        if (input[0] == '\n')
         {
-            strncpy(last_input, input, min(strlen(input), BUFSIZ));
-            memset(last_input, 0, BUFSIZ * sizeof(char));
-        }
-
-        if ((fgets(input, BUFSIZ, stdin)) == NULL)
-        { // or gets(input, BUFSIZ);
-            fprintf(stderr, "no command entered\n");
+            fprintf(stderr, "No command entered\n");
             exit(1);
         }
-        input[strlen(input) - 1] = '\0'; // wipe out newline at end of string
+
+        input[strlen(input) - 1] = 0; // wipe out newline at end of string
         // printf("input was: '%s'\n", input);
         // printf("last_input was: '%s'\n", last_input);
         if (strncmp(input, "exit", 4) == 0)
@@ -104,11 +103,19 @@ int main(int argc, const char *argv[])
             finished = true;
         }
         else if (strncmp(input, "!!", 2) == 0)
-        { // check for history command
-          // TODO
+        {
+            if (strlen(last_input) == 0)
+            {
+                printf("No commands in history\n");
+            }
+            else
+            {
+                execute(last_input);
+            }
         }
         else
         {
+            strcpy(last_input, input);
             execute(input);
         }
     }
